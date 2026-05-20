687bad109d17d4b1f63632ea9e1b1c66c4afc8b6
gmlaks91-lgtm <gmlaks91@gmail.com>
2026-05-20 09:57:02 +0900

    Vercel 배포 PWA 파일 누락 오류 해결

--- src/features/pwa/lib/pwa-precache-manifest-transform.ts
+++ src/features/pwa/lib/pwa-precache-manifest-transform.ts
@@ -1,6 +1,6 @@
 import type { ManifestTransform } from "workbox-build";
 
-import { HEAVY_STATIC_ASSET_PATTERN } from "@/features/pwa/lib/pwa-workbox-runtime-caching";
+import { HEAVY_STATIC_ASSET_PATTERN } from "./pwa-workbox-runtime-caching";
 
 /** 빌드 매니페스트에서 무거운 에셋을 프리캐시 목록에서 제거 */
 export const stripHeavyAssetsFromPrecache: ManifestTransform = async (manifestEntries) => {
